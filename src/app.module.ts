a0341b7859fb65e4a7dac36eb9f8837340661dce
northmantar <northmantar@naver.com>
2025-12-06 12:58:22 +0900

    fix: 누락 모듈 추가가

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,8 @@ import { ConfigurationService } from 'config/configuration.service';
 import { Vocabulary } from 'entities/vocabulary.entity';
 import { Grammar } from 'entities/grammar.entity';
 import { Honorific } from 'entities/honorific.entity';
+import { RiAdverb } from 'entities/ri-adverb.entity';
+import { Onomatopoeia } from 'entities/onomatopoeia.entity';
 
 @Module({
   imports: [
@@ -16,11 +18,11 @@ import { Honorific } from 'entities/honorific.entity';
       useFactory: (configurationService: ConfigurationService) => ({
         type: 'mysql',
         ...configurationService.database,
-        entities: [Vocabulary, Grammar, Honorific],
+        entities: [Vocabulary, Grammar, Honorific, RiAdverb, Onomatopoeia],
       }),
       inject: [ConfigurationService],
     }),
-    TypeOrmModule.forFeature([Vocabulary, Grammar, Honorific]),
+    TypeOrmModule.forFeature([Vocabulary, Grammar, Honorific, RiAdverb, Onomatopoeia]),
   ],
   controllers: [AppController],
   providers: [AppService],
